9eb63dffc900a7c7557cf7edde88748e411da9ef
beaker113 <43103384+beaker113@users.noreply.github.com>
2019-03-02 13:13:32 -0800

    fixed requests, but not posting data

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,19 +1,36 @@
 $("#accept").on("click", function() {
+    console.log("im working")
     $.ajax({
         headers: {
-          "Content-Type": "application/json"
+            "Content-Type": "application/json"
         },
-        type: "POST",
-        url: "/api/examples",
-        data: JSON.stringify(example)
-      });
-})
+        type: "Get",
+        url: "/api/requests",
+        data: JSON.stringify(request)
+    }).then(function(){
 
-$("#deny").on("click", function(id) {
+        $.ajax({
+        
+            headers: {
+              "Content-Type": "application/json"
+            },
+            type: "POST",
+            url: "/api/examples",
+            data: JSON.stringify(example)
+          });
+    })
+    })
+    
+
+$(".delete").on("click", function(event) {
+    
     
         $.ajax({
-          url: "/api/requests/" + id,
+          url: "/api/requests/" + event.currentTarget.id,
           type: "DELETE"
-        });
+        })
+       .then(function() {
+           location.reload();
+       }) 
       })
    
