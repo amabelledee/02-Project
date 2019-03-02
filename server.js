dfbd63cf1e06b0190fd43240eaa2c82ce3bbd57d
Dave Cox <davedvst@gmail.com>
2019-03-02 09:49:22 -0800

    authentication

--- server.js
+++ server.js
@@ -1,47 +1,68 @@
-require("dotenv").config();
 var express = require("express");
+var app = express();
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var env = require("dotenv").load();
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
+//For BodyParser
+app.use(
+  bodyParser.urlencoded({
+    extended: true
   })
 );
-app.set("view engine", "handlebars");
+app.use(bodyParser.json());
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// For Passport
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-var syncOptions = { force: false };
+//For Handlebars
+app.set("views", "./app/views");
+app.engine(
+  "hbs",
+  exphbs({
+    extname: ".hbs"
+  })
+);
+app.set("view engine", ".hbs");
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.get("/", function(req, res) {
+  res.send("Welcome to Passport with Sequelize");
 });
 
-module.exports = app;
+//Models
+var models = require("./app/models");
+//Routes
+
+var authRoute = require("./app/routes/auth.js")(app, passport);
+app.use(express.static("public"));
+//load passport strategies
+
+require("./app/config/passport/passport.js")(passport, models.user);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+//Sync Database
+
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log("Connected! Database and Port 5000");
+  })
+  .catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!");
+  });
+
+app.listen(5000, function(err) {
+  if (!err) console.log("Site is live");
+  else console.log(err);
+});
